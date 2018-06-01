20fd4c00f4b7cc7f67d832627d31f582b52c0df2
Claudiu Constantin Bogdan <bogdan.claudiu93@gmail.com>
2018-06-01 21:48:35 +0300

    Created a stating screen and a scene.

--- SceneNavegation/desktop/src/com/claudiubogdan/scene/desktop/DesktopLauncher.java
+++ SceneNavegation/desktop/src/com/claudiubogdan/scene/desktop/DesktopLauncher.java
@@ -3,6 +3,7 @@ package com.claudiubogdan.scene.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.claudiubogdan.scene.SceneNavegator;
+import drop.Drop;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
