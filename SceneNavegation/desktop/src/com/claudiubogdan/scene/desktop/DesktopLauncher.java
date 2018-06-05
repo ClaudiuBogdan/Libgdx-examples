839466323b7caeb031154ecaebe8fe7f15a0694b
Claudiu Constantin Bogdan <bogdan.claudiu93@gmail.com>
2018-06-05 20:48:01 +0300

    Imported project from deeep game.

--- SceneNavegation/desktop/src/com/claudiubogdan/scene/desktop/DesktopLauncher.java
+++ SceneNavegation/desktop/src/com/claudiubogdan/scene/desktop/DesktopLauncher.java
@@ -2,12 +2,17 @@ package com.claudiubogdan.scene.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.claudiubogdan.scene.SceneNavegator;
-import drop.Drop;
+import spaceglad.*;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+/*	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new SceneNavegator(), config);
-	}
+	}*/
+public static void main(String[] arg) {
+	LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+	config.width = (int) Core.VIRTUAL_WIDTH;
+	config.height = (int) Core.VIRTUAL_HEIGHT;
+	new LwjglApplication(new Core(), config); /**/
+}
 }
